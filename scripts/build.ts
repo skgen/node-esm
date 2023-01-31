7e709b63a7964ca2732c15b407e7e1c5f0412c39
Lucas Chapuis <l.chapuis@patriarche.fr>
2023-01-31 14:34:56 +0100

    build: fix build script tsconfig resolution to more stable path

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,6 +1,5 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { build } from 'esbuild';
-import path from 'path';
 import { root } from './partials/resolve.js';
 
 const entrypoint = root('src/main.ts');
@@ -13,7 +12,7 @@ await build({
   platform: 'node',
   format: 'esm',
   minify: true,
-  tsconfig: path.resolve(process.cwd(), 'tsconfig.app.json'),
+  tsconfig: root('tsconfig.app.json'),
   banner: {
     js: 'import { createRequire } from \'module\';const require = createRequire(import.meta.url);',
   },
