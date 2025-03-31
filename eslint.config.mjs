baeeecda4e63bbed056e50fa594a3874caf8fa1f
Lucas Chapuis <chapuis.lucas74@gmail.com>
2025-03-31 16:54:12 +0200

    feat: forces newline between import groups

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -25,7 +25,7 @@ const config = antfu(
               'object',
               'unknown',
             ],
-            newlinesBetween: 'ignore',
+            newlinesBetween: 'always',
             internalPattern: ['^@/.+'],
             order: 'asc',
             type: 'natural',
